4252cae675e07c93e26ae804423d0462cd628e1f
rjhd1990 <coderrj14@gmail.com>
2025-12-23 08:13:11 +0000

    minor changes

--- app/stream_items.py
+++ app/stream_items.py
@@ -1,15 +1,14 @@
 class StreamItems:
     def __init__(self, value_items: list = []):
         self.value_items = value_items
-        self.last_item_ms = 0
-        self.last_item_seq = 0
+        self.last_id :tuple[int, int] = (0, 0)
 
     def xadd(self, sid: str, item: dict = {}):
         ms, seq = sid.split("-")
         if sid == "0-0":
             return False, "ERR The ID specified in XADD must be greater than 0-0"
-        if int(ms) < self.last_item_ms or (
-            int(ms) == self.last_item_ms and int(seq) <= self.last_item_seq
+        if int(ms) < self.last_id[0] or (
+            int(ms) == self.last_id[0] and int(seq) <= self.last_id[1]
         ):
             return (
                 False,
@@ -17,8 +16,7 @@ class StreamItems:
             )
         item["id"] = sid
         self.value_items.append(item)
-        self.last_item_ms = int(ms)
-        self.last_item_seq = int(seq)
+        self.last_id = [int(ms),  int(seq)]
         return True, sid
 
     def view_item(self):
